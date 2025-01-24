bf4d88270aed7c10c8c6decbc1aff84050a41925
Shinjan saha <shinjansaha02@gmail.com>
2025-01-24 19:43:27 +0530

    All Socials Updated

--- consts/SocialLinks.ts
+++ consts/SocialLinks.ts
@@ -1,7 +1,7 @@
 export const SOCIAL_LINKS = {
-    FACEBOOK: 'https://www.facebook.com/droidfest2025',
-    LINKEDIN: 'https://www.linkedin.com/company/droidfest2025',
-    TWITTER: 'https://twitter.com/droidfest2025',
-    INSTAGRAM: 'https://www.instagram.com/droidfest2025',
-    YOUTUBE: 'https://www.youtube.com/c/droidfest2025'
+    FACEBOOK: 'https://www.facebook.com/share/1EXDaU39FF/',
+    LINKEDIN: 'https://www.linkedin.com/company/kotlin-kolkata/posts/?feedView=all',
+    TWITTER: 'https://x.com/KotlinKolkata',
+    INSTAGRAM: 'https://www.instagram.com/kotlinkolkata/',
+    YOUTUBE: 'https://youtube.com/@kotlinkolkata9397?si=_GHQGJ994pleRjwW'
 } as const;
