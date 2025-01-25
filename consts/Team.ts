af4cfe49bc985a4d989c46287b72726a4ffa131d
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2025-01-25 15:05:32 +0530

    add: team information and images

--- consts/Team.ts
+++ consts/Team.ts
@@ -6,58 +6,249 @@ export interface TeamMember {
   linkedin?: string;
   twitter?: string;
   github?: string;
+  instagram?: string;
   website?: string;
 }
 
 export const team: TeamMember[] = [
+  // Organisers
   {
-    firstName: 'Johnathon',
-    lastName: 'Doe',
-    role: 'Android Developer',
-    image: '/team-test.png',
-    linkedin: 'https://linkedin.com/in/johndoe',
-    twitter: 'https://twitter.com/johndoe',
-    github: 'https://github.com/johndoe',
-    website: 'https://johndoe.com',
+    firstName: "Supratim",
+    lastName: "Dhara",
+    role: "Organiser",
+    image: "/team/supratim-dhara.png",
+    linkedin: "https://www.linkedin.com/in/supratimdhara/",
+    twitter: "https://x.com/supratimtwt",
+    github: "https://github.com/supratim1609",
+    instagram: "https://www.instagram.com/dhara_apk/"
   },
   {
-    firstName: 'Janice',
-    lastName: 'Smith',
-    role: 'UI/UX Designer',
-    image: '/team-test.png',
-    linkedin: 'https://linkedin.com/in/janicesmith',
-    twitter: 'https://twitter.com/janicesmith',
-    github: 'https://github.com/janicesmith',
-    website: 'https://janicesmith.com',
+    firstName: "Shreya",
+    lastName: "Moulick",
+    role: "Organiser",
+    image: "/team/shreya-moulik.png",
+    linkedin: "https://www.linkedin.com/in/shreyamoulick",
+    twitter: "https://twitter.com/shreya_moulick",
+    github: "https://github.com/shre-byte-11",
+    instagram: "https://www.instagram.com/shreya_moulick/"
   },
   {
-    firstName: 'Michael',
-    lastName: 'Brown',
-    role: 'Full Stack Developer',
-    image: '/team-test.png',
-    linkedin: 'https://linkedin.com/in/michaelbrown',
-    twitter: 'https://twitter.com/michaelbrown',
-    github: 'https://github.com/michaelbrown',
-    website: 'https://michaelbrown.com',
+    firstName: "Tithi",
+    lastName: "Bose",
+    role: "Organiser",
+    image: "/team/tithi-bose.png",
+    linkedin: "www.linkedin.com/in/tithib",
+    twitter: "www.x.com/TithiBose7",
+    github: "www.github.com/TithiB",
+    instagram: "www.instagram.com/itsmetithibose"
+  },
+  // PR and Outreach team
+  {
+    firstName: "Rahul",
+    lastName: "Khandait",
+    role: "PR and Outreach team",
+    image: "/team/rahul-khandait.png",
+    linkedin: "https://www.linkedin.com/in/rahulkhandait/",
+    twitter: "https://x.com/rahulkhandait_",
+    github: "https://github.com/rahulkhandait-sde",
+    instagram: "https://www.instagram.com/rahulkhandait_sde"
   },
   {
-    firstName: 'Emily',
-    lastName: 'Johnson',
-    role: 'Data Scientist',
-    image: '/team-test.png',
-    linkedin: 'https://linkedin.com/in/emilyjohnson',
-    twitter: 'https://twitter.com/emilyjohnson',
-    github: 'https://github.com/emilyjohnson',
-    website: 'https://emilyjohnson.com',
+    firstName: "Anurag",
+    lastName: "Roy",
+    role: "PR and Outreach team",
+    image: "/team/anurag-roy.png",
+    linkedin: "https://www.linkedin.com/in/anuragroy485/",
+    twitter: "https://x.com/anuragroy485/",
+    github: "https://github.com/anuragroy485",
+    instagram: "https://www.instagram.com/itsmeanuragroy"
   },
   {
-    firstName: 'David',
-    lastName: 'Williams',
-    role: 'DevOps Engineer',
-    image: '/team-test.png',
-    linkedin: 'https://linkedin.com/in/davidwilliams',
-    twitter: 'https://twitter.com/davidwilliams',
-    github: 'https://github.com/davidwilliams',
-    website: 'https://davidwilliams.com',
+    firstName: "Sarmistha",
+    lastName: "Ghosh",
+    role: "PR and Outreach team",
+    image: "/team/sarmistha-ghosh.png",
+    linkedin: "https://www.linkedin.com/in/sarmistha-ghosh-923171281?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://x.com/sarmisthasoumi",
+    github: "https://github.com/sarmisthaghosh2005",
+    instagram: "https://www.instagram.com/sarmistha.ghosh_2005/"
   },
+  {
+    firstName: "Debarati",
+    lastName: "Biswas",
+    role: "PR and Outreach team",
+    image: "/team/debarati-biswas.png",
+    linkedin: "https://www.linkedin.com/in/debarati-biswas-488b4a244?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/thississsssdb__?igsh=dTU0Z2preXRocW00"
+  },
+  {
+    firstName: "Rupsa",
+    lastName: "Das",
+    role: "PR and Outreach team",
+    image: "/team/rupsa-das.png",
+    linkedin: "https://www.linkedin.com/in/rupsha-das-b6b5a8253",
+    github: "https://github.com/Rupsha-Das",
+    twitter: "https://x.com/das_rupsha18562?t=LXLsy0laM_nmUJA2ZVU3sg&s=09",
+    instagram: "https://www.instagram.com/rupsha.py?igsh=MTlvN2NndnhjZnFzeQ=="
+  },
+  // Web dev team
+  {
+    firstName: "Subhadip",
+    lastName: "Saha",
+    role: "Web dev team",
+    image: "/team/subhadip-saha.png",
+    linkedin: "https://www.linkedin.com/in/subhadipsahaofficial",
+    twitter: "https://twitter.com/SubhadipDev",
+    github: "https://twitter.com/SubhadipDev",
+    instagram: "https://www.instagram.com/subh05sus_"
+  },
+  {
+    firstName: "Subinoy",
+    lastName: "Biswas",
+    role: "Web dev team",
+    image: "/team/subinoy-biswas.png",
+    linkedin: "https://www.linkedin.com/in/heysubinoy/",
+    twitter: "https://x.com/heysubinoy",
+    github: "https://github.com/subinoybiswas",
+    instagram: "https://www.instagram.com/biswas.subinoy"
+  },
+  {
+    firstName: "Shinjan",
+    lastName: "Saha",
+    role: "Web dev team",
+    image: "/team/shinjan-saha.png",
+    linkedin: "https://www.linkedin.com/in/shinjan-saha-404064248/",
+    twitter: "https://x.com/Eq3EyjUGyja9bCM",
+    github: "https://github.com/Shinjan-saha",
+    instagram: "https://www.instagram.com/code_mafia_?igsh=eml1cWRnd240N2Ew"
+  },
+  {
+    firstName: "Saurabh",
+    lastName: "Sharma",
+    role: "Web dev team",
+    image: "/team/saurabh-sharma.png",
+    linkedin: "https://linkedin.com/in/esskayesss",
+    twitter: "https://x.com/esskayesss_",
+    github: "https://github.com/esskayesss",
+    instagram: "https://instagram.com/esskayesss_"
+  },
+  // Photography team
+  {
+    firstName: "Gurdil",
+    lastName: "Singh",
+    role: "Photography team",
+    image: "/team/gurdil-singh.png",
+    linkedin: "https://www.linkedin.com/in/gurdil-singh-05124b339?",
+    instagram: "https://www.instagram.com/gurdil.singh?"
+  },
+  {
+    firstName: "Soumoydip",
+    lastName: "Halder",
+    role: "Photography team",
+    image: "/team/soumoydip-halder.png",
+    linkedin: "https://www.linkedin.com/in/soumoydiphalder/",
+    twitter: "https://x.com/soulogs",
+    github: "https://github.com/soumoydip",
+    instagram: "https://www.instagram.com/soulogss/"
+  },
+  {
+    firstName: "Bhumika",
+    lastName: "Das",
+    role: "Photography team",
+    image: "/team/bhumika-das.png",
+    linkedin: "https://www.linkedin.com/in/bhumika-das-074189326?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://x.com/Bhuumiiiikaaa?t=0cxkniclIImO41CAVdstmA&s=09",
+    github: "https://github.com/bhuumiiiika",
+    instagram: "https://www.instagram.com/bhumika_visuals?igsh=MTIzM3MxbHNkdTNycA=="
+  },
+  {
+    firstName: "Anwesha",
+    lastName: "Choudhury",
+    role: "Photography team",
+    image: "/team/anwesha-choudhury.png",
+    linkedin: "https://www.linkedin.com/in/anwesha-choudhury-5b45b3325?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    instagram: "https://www.instagram.com/anweshaaa._27?igsh=bjZ2ODByc21oNnI4"
+  },
+  {
+    firstName: "Sambit",
+    lastName: "Sarkar",
+    role: "Photography team",
+    image: "/team/sambit-sarkar.png",
+    linkedin: "https://www.linkedin.com/in/sambit-sarkar-12b041212/",
+    twitter: "https://x.com/Sambits28225539?t=1dFPhrgsfhR9tdAIGMkrCw&s=09",
+    github: "https://github.com/Sscuber03",
+    instagram: "https://www.instagram.com/ss_cuber2?igsh=dW00aGFvMTFrZWJm"
+  },
+  // Design team
+  {
+    firstName: "Adrija",
+    lastName: "Karmakar",
+    role: "Design team",
+    image: "/team/adrija-karmakar.png",
+    linkedin: "https://www.linkedin.com/in/adrija-karmakar-9a639028a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    twitter: "https://x.com/karm73902?t=K0vRrtsyKocyP4Ia4BxY0Q&s=09",
+    github: "https://github.com/Adrija78",
+    instagram: "https://www.instagram.com/authentic_ahel?igsh=M3dvNjdlc3c3NjZj"
+  },
+  {
+    firstName: "Srijan",
+    lastName: "Paul",
+    role: "Design team",
+    image: "/team/srijan-paul.png",
+    linkedin: "https://www.linkedin.com/in/srijan-paul-547354260/",
+    twitter: "https://x.com/iampaulsrijan",
+    github: "https://github.com/paul-srijan",
+    instagram: "https://instagram.com/iampaulsrijan/"
+  },
+  // Anchoring team
+  {
+    firstName: "Ratnadeep",
+    lastName: "Mukherjee",
+    role: "Anchoring team",
+    image: "/team/ratnadeep-mukherjee.png",
+    linkedin: "https://www.linkedin.com/in/ratnadeep-mukherjee-8b158b21b",
+    twitter: "https://x.com/Ratnadeep240102",
+    github: "https://github.com/RatnadeepMukherjee",
+    instagram: "https://www.instagram.com/ugottt_ratnadeep?igsh=bG1zZm5tb3BrdHZ6"
+  },
+  {
+    firstName: "Sumita",
+    lastName: "Pathak",
+    role: "Anchoring team",
+    image: "/team/sumita-pathak.png",
+    linkedin: "https://www.linkedin.com/in/sumita-pathak-91699a215",
+    twitter: "https://x.com/SumitaPathak1?t=kki3QR7cTXIXKZdSZnZGTQ&s=09",
+    github: "https://github.com/httpsumita",
+    instagram: "https://www.instagram.com/codefit.io"
+  },
+  {
+    firstName: "Ankit",
+    lastName: "Kumar Gupta",
+    role: "Anchoring team",
+    image: "/team/ankit-kumar-gupta.png",
+    linkedin: "https://www.linkedin.com/in/ankit-kumar-gupta-6ba724266/",
+    github: "https://github.com/Ankit-03G",
+    instagram: "https://www.instagram.com/ankit0_3?igsh=MmtkbnAxbjBqMHly"
+  },
+  // Management team
+  {
+    firstName: "Abantika",
+    lastName: "Roy",
+    role: "Management team",
+    image: "/team/abantika-roy.png",
+    linkedin: "https://www.linkedin.com/in/abantika-roy-a4748025b/",
+    twitter: "https://x.com/AbantikaRo95982",
+    github: "https://github.com/Aritri05",
+    instagram: "https://www.instagram.com/abantika_r0y?igsh=MXQ5ejI0Y2hzNzJibQ=="
+  },
+  {
+    firstName: "Aryan",
+    lastName: "Chettri",
+    role: "Anchoring team",
+    image: "/team/aryan-chettri.png",
+    linkedin: "www.linkedin.com/in/your1copywriter",
+    twitter: "https://x.com/aryanchettri_X",
+    github: "https://github.com/aryan-chettri",
+    instagram: "https://www.instagram.com/_.chettri_/"
+  }
 ];
