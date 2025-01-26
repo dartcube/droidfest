3becb92416b360679f8d985b98142640771e5129
Shinjan saha <shinjansaha02@gmail.com>
2025-01-26 10:40:39 +0530

    removed Unnecessary Pages as we are using components in same page only

--- app/partners/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function PartnersPage() {
-    return (
-      <div className="flex flex-grow h-full items-center justify-center">
-        <h1 className="text-3xl font-semibold">Partners Page</h1>
-      </div>
-    );
-}
-  
--- app/workshops/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function WorkshopsPage() {
-    return (
-      <div className="flex flex-grow h-full items-center justify-center">
-        <h1 className="text-3xl font-semibold">Workshops Page</h1>
-      </div>
-    );
-  }
-  
