248e635e60b8497a704b623f6b8cddebd9da62fa
Shinjan saha <shinjansaha02@gmail.com>
2025-01-26 10:12:12 +0530

    About-Us Pic Added

--- consts/PreviousYearImages.ts
+++ consts/PreviousYearImages.ts
@@ -1,10 +1,9 @@
 export const PreviousYearImageUrls = [
-    "/landscape.png",
-    "/landscape.png",
-    "/landscape.png",
-    "/landscape.png",
-    "/landscape.png",
-    "/landscape.png",
-    "/landscape.png",
-    "/landscape.png",
+    "/AIV01939.JPG",
+    "/AIV02121.JPG",
+    "/AIV02153.JPG",
+    "/AIV02127.JPG",
+    "/AIV02043.JPG",
+    "/AIV02113.JPG",
+    "/AIV02115.JPG",
 ];
