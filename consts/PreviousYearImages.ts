7a6025d811730efe65d9632974563b0ab4918663
Shinjan saha <shinjansaha02@gmail.com>
2025-01-26 19:26:24 +0530

    About-us images grid updated

--- consts/PreviousYearImages.ts
+++ consts/PreviousYearImages.ts
@@ -1,9 +1,9 @@
 export const PreviousYearImageUrls = [
     "/AIV01939.webp",
-    "/AIV02121.webp",
+    "/AIV02105.webp",
     "/AIV02153.webp",
-    "/AIV02127.webp",
+    "/aboutusgrid-1.webp",
     "/AIV02043.webp",
-    "/AIV02113.webp",
-    "/AIV02115.webp",
+    "/AIV02086.webp",
+    "/AIV02039.webp",
 ];
