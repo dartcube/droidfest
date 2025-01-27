2c2b16d8473154bd4814fae4209446ad18b1ea17
Subinoy Biswas <115915591+subinoybiswas@users.noreply.github.com>
2025-01-27 13:00:35 +0530

    update: specify titles for KKUG and Droidfest'25 organisers

--- components/team/team.tsx
+++ components/team/team.tsx
@@ -17,7 +17,7 @@ export default function Team({
       className="relative py-16 px-8 bg-cover bg-center bg-no-repeat"
     >
       <h2 className="text-5xl font-bold text-center text-transparent bg-clip-text bg-text-gradient mb-12 py-1">
-        {isOrganiser && 'Organisers'} {isEventOrganiser && 'Event Organisers'} {isTeam && 'Team'}
+        {isOrganiser && 'KKUG Organisers'} {isEventOrganiser && "Droidfest'25 Organisers"} {isTeam && 'Team'}
       </h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 max-w-6xl mx-auto">
         {isTeam && team.filter(member => member.role != 'Organiser' && member.role != "Event Organiser").map((member, index) => (
