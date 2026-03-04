227d23891e2f99bbd31585e464378da660548ccf
devMonkRahul <rahulpal2023iem@gmail.com>
2026-03-04 21:12:11 +0530

    Added the venue and ticket details

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Link from "next/link";
 import { SOCIAL_LINKS } from "@/consts/SocialLinks";
-import { BrandLogo } from "../ui/brand/logo";
+// import { BrandLogo } from "../ui/brand/logo";
 import { Facebook, Twitter, Linkedin, Instagram, Youtube } from "lucide-react";
 
 const Footer: React.FC = () => {
   return (
     <footer className=" bg-transparent  text-white py-6">
       <div className="container mx-auto text-center flex w-full flex-col items-center gap-3">
+        <img src="/brand26.svg" alt="Brand Logo" className="w-64 h-auto" />
         {/* <BrandLogo className={"w-64"} /> */}
         {/* <Button className="border rounded-full">Contact Us</Button> */}
         <div className="h-0.5 w-full bg-white/20 my-4" />
